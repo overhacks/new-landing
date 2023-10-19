a20bbcdb06d026f0e61a00b857dc8ea94a3f8172
Dmitry Kodolbenko <115988813+DmitryKodolbenko@users.noreply.github.com>
2023-10-19 05:48:41 +0400

    Styled swiper in progress

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,20 +1,83 @@
 import React, { useEffect, useState } from "react";
-import { HomeScreenContainer, HomeScreenContent, } from "./styles";
+import {
+  HomeScreenContainer,
+  HomeScreenContent,
+  StyledSlide,
+  StyledSwiper,
+} from "./styles";
 import Header from "../../components/Header";
 // import InvitePopup from 'components/newDesignComponents/invitePopup'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Virtual } from "swiper/modules";
+import "swiper/css/virtual";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 function HomeScreen() {
-  
-  
+  const slides = [
+    "Slide 1",
+    "Slide 2",
+    "Slide 3",
+    "Slide 4",
+    "Slide 5",
+    "Slide 6",
+    "Slide 7",
+    "Slide 8",
+    "Slide 9",
+    "Slide 10",
+    "Slide 11",
+    "Slide 12",
+    "Slide 13",
+    "Slide 14",
+    "Slide 15",
+    "Slide 16",
+    "Slide 17",
+    "Slide 18",
+    "Slide 19",
+    "Slide 20",
+  ];
+
   return (
     <>
-    <HomeScreenContainer>
-        <Header/>
+      <HomeScreenContainer>
+        <Header />
         <HomeScreenContent>
+          
+            <StyledSwiper
+              modules={[Virtual]}
+              spaceBetween={40}
+              slidesPerView={3}
+              virtual
+              loop={true}
+              centeredSlides={true}
+              breakpoints= {{
 
+                640: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1080: {
+                    slidesPerView: 3,
+                },
+                1280: {
+                    slidesPerView: 4,
+                }}
+            }
+            >
+              {slides.map((slideContent, index) => (
+                <StyledSlide key={slideContent} virtualIndex={index}>
+                  <h1>{slideContent}</h1>
+                </StyledSlide>
+              ))}
+            </StyledSwiper>
         </HomeScreenContent>
-    </HomeScreenContainer>
+      </HomeScreenContainer>
     </>
   );
 }
--- src/screens/HomeScreen/styles.ts
+++ src/screens/HomeScreen/styles.ts
@@ -1,4 +1,6 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export const HomeScreenContainer = styled.div`
   flex: 1;
@@ -6,9 +8,29 @@ export const HomeScreenContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: column;
-
 `;
 
 export const HomeScreenContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
-`
+export const StyledSwiper = styled(Swiper)`
+  width: 100%;
+  height: 400px;
+  margin: 40px 0;
+  padding: 45px 0;
+`;
+
+export const StyledSlide = styled(SwiperSlide)`
+    opacity: 0.4;
+    overflow: hidden;
+    width: 100px;
+    height: 250px;
+    background-color: white;
+
+    display: flex;
+    flex-direction: column;
+`;
