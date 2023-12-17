70081b98761b4802a23ad616158d14f713fb0141
Dmitry Kodolbenko <115988813+DmitryKodolbenko@users.noreply.github.com>
2023-12-17 15:58:14 +0400

    feat: mobile popup

--- src/components/HackatonsMobile/index.tsx
+++ src/components/HackatonsMobile/index.tsx
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Banner,
   ButtonText,
+  ButtonTextPopup,
+  CancelBtn,
+  CardContentWrapper,
   CardsBullet,
   CardsPoint,
+  Description,
   HackatonCard,
   HackatonHeader,
   HackatonRow,
+  HackatonRowMobile,
   HackatonsColumn,
   HackatonsMobileSection,
   InfoColumn,
   InfoData,
   InfoLabel,
   InfoRow,
+  LargeApplyButton,
+  LargeApplyWrapper,
+  Popup,
+  PopupContentWrapper,
+  PopupStyledCross,
   PrizePoolWrapper,
   PrizeValue,
   SeeButtonWrapper,
   SeeMoreButton,
+  TextWrapper,
   Topics,
 } from "./styles";
 import { Hackathon } from "../../api/client";
 
 import SeeMoreBackground from "../../assets/img/seeMore.svg";
+import MobileCrossSVG from "../../assets/img/MobileCross.svg";
+import LargeApplyButtonSVG from "../../assets/img/largeApply.svg";
 
 interface HackatonsMobileProps {
   hackatons: Hackathon[];
@@ -32,56 +45,139 @@ const HackatonsMobile: React.FC<HackatonsMobileProps> = ({
   hackatons,
   isOngoing,
 }) => {
+  const ColumnRef = useRef(null);
+
   const [visibleCards, setVisibleCards] = useState(1);
 
   const handleSeeMoreClick = () => {
     setVisibleCards((prevVisibleCards) => prevVisibleCards + 2);
   };
 
+  const scrollToElement = (targetRef: React.RefObject<HTMLElement>) => {
+    if (targetRef.current) {
+      targetRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  const [selectedHackatonIdMobile, setSelectedHackatonIdMobile] =
+    useState(null);
+  const [popupMobileOpen, setPopupMobileOpen] = useState(false);
+
+  const handlePopupOpen = (id: any) => {
+    if (popupMobileOpen) {
+      setPopupMobileOpen(false);
+      setSelectedHackatonIdMobile(null);
+    } else if (!popupMobileOpen) {
+      scrollToElement(ColumnRef);
+
+      setPopupMobileOpen(true);
+      setSelectedHackatonIdMobile(id);
+    }
+  };
+
+  const [currentHackaton, setCurrentHackaton] = useState(hackatons[0]);
+
+  useEffect(() => {
+    const updatedCurrentHackaton =
+      hackatons.find((hackaton) => hackaton.id === selectedHackatonIdMobile) ||
+      hackatons[0];
+    setCurrentHackaton(updatedCurrentHackaton);
+  }, [selectedHackatonIdMobile, hackatons]);
+
+  const [paragraphs, setParagraphs] = useState([""]);
+
+  useEffect(() => {
+    if (currentHackaton) {
+      setParagraphs(currentHackaton?.description.split(/<\/p>\s*<p>/));
+    } else {
+      setParagraphs([""]);
+    }
+  }, [currentHackaton]);
+
   return (
     <HackatonsMobileSection>
-      <HackatonsColumn>
+      <HackatonsColumn ref={ColumnRef}>
         {hackatons.slice(0, visibleCards).map((hackathon, index) => (
-          <HackatonCard key={hackathon.id}>
-            <HackatonHeader>{hackathon.name}</HackatonHeader>
-            <HackatonRow>
-              <Banner imgSrc={hackathon.imageUrl} width="45%" />
-              <Topics>
-                {hackathon.topics.slice(0, 3).map((topic) => (
-                  <CardsPoint>
-                    {" "}
-                    <CardsBullet>// </CardsBullet> {topic}
-                  </CardsPoint>
-                ))}
-              </Topics>
-            </HackatonRow>
+          <HackatonCard
+            key={hackathon.id}
+            onClick={() => handlePopupOpen(hackathon.id)}
+          >
+            <CardContentWrapper>
+              <HackatonHeader>{hackathon.name}</HackatonHeader>
+              <HackatonRow>
+                <Banner imgSrc={hackathon.imageUrl} width="45%" />
+                <Topics>
+                  {hackathon.topics.slice(0, 3).map((topic) => (
+                    <CardsPoint>
+                      {" "}
+                      <CardsBullet>// </CardsBullet> {topic}
+                    </CardsPoint>
+                  ))}
+                </Topics>
+              </HackatonRow>
 
-            <HackatonRow>
-              <InfoRow>
-                <InfoColumn>
-                  <InfoLabel>Format</InfoLabel>
-                  <InfoData>{hackathon.format ?? "Online"}</InfoData>
-                </InfoColumn>
-                <InfoColumn>
-                  <InfoLabel>Start Date</InfoLabel>
-                  <InfoData>
-                    {new Date(hackathon.startDate)
-                      .toLocaleDateString("en-US", {
-                        month: "2-digit",
-                        day: "2-digit",
-                        year: "numeric",
-                      })
-                      .replace(/\//g, ".")}
-                  </InfoData>
-                </InfoColumn>
-              </InfoRow>
+              <HackatonRow>
+                <InfoRow>
+                  <InfoColumn>
+                    <InfoLabel>Format</InfoLabel>
+                    <InfoData>{hackathon.format ?? "Online"}</InfoData>
+                  </InfoColumn>
+                  <InfoColumn>
+                    <InfoLabel>Start Date</InfoLabel>
+                    <InfoData>
+                      {new Date(hackathon.startDate)
+                        .toLocaleDateString("en-US", {
+                          month: "2-digit",
+                          day: "2-digit",
+                          year: "numeric",
+                        })
+                        .replace(/\//g, ".")}
+                    </InfoData>
+                  </InfoColumn>
+                </InfoRow>
 
-              <PrizePoolWrapper>
-                <PrizeValue>{hackathon.prize}</PrizeValue>
-              </PrizePoolWrapper>
-            </HackatonRow>
+                <PrizePoolWrapper>
+                  <PrizeValue>{hackathon.prize}</PrizeValue>
+                </PrizePoolWrapper>
+              </HackatonRow>
+            </CardContentWrapper>
           </HackatonCard>
         ))}
+
+        {popupMobileOpen && (
+          <Popup>
+            <PopupContentWrapper>
+              <HackatonRowMobile>
+                <HackatonHeader>{currentHackaton.name}</HackatonHeader>
+                <CancelBtn
+                  onClick={() => {
+                    setPopupMobileOpen(false);
+                  }}
+                >
+                  <PopupStyledCross imgSrc={MobileCrossSVG} />
+                </CancelBtn>
+              </HackatonRowMobile>
+              <HackatonRow>
+                <Description>
+                  {paragraphs &&
+                    paragraphs.map((paragraph) => (
+                      <TextWrapper
+                        dangerouslySetInnerHTML={{ __html: paragraph }}
+                      ></TextWrapper>
+                    ))}
+                </Description>
+              </HackatonRow>
+              <LargeApplyWrapper>
+                <LargeApplyButton
+                  backgroundImg={LargeApplyButtonSVG}
+                  href="https://t.me/OverhacksBot"
+                >
+                  <ButtonTextPopup>Apply</ButtonTextPopup>
+                </LargeApplyButton>
+              </LargeApplyWrapper>
+            </PopupContentWrapper>
+          </Popup>
+        )}
       </HackatonsColumn>
 
       {visibleCards < hackatons.length && (
--- src/components/HackatonsMobile/styles.ts
+++ src/components/HackatonsMobile/styles.ts
@@ -18,6 +18,193 @@ export const HackatonsColumn = styled.div`
   justify-content: flex-start;
   align-items: center;
   gap: 40px;
+
+  position: relative;
+`;
+
+export const PopupContentWrapper = styled.div`
+  width: calc(90% - 12px);
+  padding-right: 12px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  max-height: 100%;
+  overflow-x: hidden;
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    width: 9px;
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #445144;
+    border-radius: 3px;
+    width: 9px;
+  }
+
+  &::-webkit-scrollbar-thumb:hover {
+    background-color: #445144a8;
+  }
+
+  @media (max-width: 576px) {
+    gap: 18px;
+  }
+  @media (min-width: 577px) {
+    gap: 24px;
+  }
+  @media (min-width: 769px) {
+    gap: 28px;
+  }
+  @media (min-width: 993px) {
+    gap: 30px;
+  }
+  @media (min-width: 1201px) {
+    gap: 30px;
+  }
+  @media (min-width: 1401px) {
+    gap: 30px;
+  }
+`;
+
+export const CardContentWrapper = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  @media (max-width: 576px) {
+    gap: 18px;
+  }
+  @media (min-width: 577px) {
+    gap: 24px;
+  }
+  @media (min-width: 769px) {
+    gap: 28px;
+  }
+  @media (min-width: 993px) {
+    gap: 30px;
+  }
+  @media (min-width: 1201px) {
+    gap: 30px;
+  }
+  @media (min-width: 1401px) {
+    gap: 30px;
+  }
+`;
+
+
+export const ButtonTextPopup = styled.p`
+  color: #dddbdb;
+  font-family: Orbitron;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: 0.45px;
+
+  @media (max-width: 576px) {
+    font-size: 17px;
+  }
+  @media (min-width: 577px) {
+    font-size: 20px;
+  }
+
+  @media (min-width: 769px) {
+    font-size: 25px;
+  }
+  @media (min-width: 993px) {
+    font-size: 15px;
+  }
+  @media (min-width: 1201px) {
+    font-size: 15px;
+  }
+  @media (min-width: 1401px) {
+    font-size: 15px;
+  }
+`;
+
+export const LargeApplyButton = styled.a<{
+  backgroundImg: any;
+}>`
+  background-color: transparent;
+  background-image: url(${(props) => props.backgroundImg});
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  text-decoration: none;
+
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  aspect-ratio: 6.5 / 1;
+
+  cursor: pointer;
+
+
+  &:hover {
+    opacity: 0.6;
+  }
+
+  
+`;
+
+
+export const LargeApplyWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+
+export const Popup = styled.div`
+  z-index: 999999999;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+
+  padding-top: 17px;
+  padding-bottom: 28px;
+  max-height: calc(100% - 45px);
+
+  background-color: rgb(18, 23, 24);
+  box-shadow: 0px 66px 100px 0px rgba(41, 151, 84, 0.25) inset,
+    0px 4px 18px 0px #299754 inset, 0px 0px 20px 0px rgba(0,0,0,0.75);
+  -webkit-backdrop-filter: blur(50px);
+  backdrop-filter: blur(50px);
+  border-radius: 24px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 18px;
+
+  @media (max-width: 576px) {
+    gap: 18px;
+  }
+  @media (min-width: 577px) {
+    gap: 24px;
+  }
+  @media (min-width: 769px) {
+    gap: 28px;
+  }
+  @media (min-width: 993px) {
+    gap: 30px;
+  }
+  @media (min-width: 1201px) {
+    gap: 30px;
+  }
+  @media (min-width: 1401px) {
+    gap: 30px;
+  }
 `;
 
 export const HackatonHeader = styled.h1`
@@ -56,6 +243,39 @@ export const HackatonRow = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: flex-start;
+`;
+
+export const HackatonRowMobile = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const CancelBtn = styled.button`
+  border: none;
+  background-color: transparent;
+  text-decoration: none;
+  cursor: pointer;
+
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+export const PopupStyledCross = styled.button<{ imgSrc: string }>`
+  background-image: url(${(props) => props.imgSrc});
+  background-color: transparent;
+  border: none;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: contain;
+  z-index: 2;
+
+  width: 30px;
+  height: 24px;
+  cursor: pointer;
 `;
 
 export const InfoRow = styled.div`
@@ -224,63 +444,96 @@ export const Banner = styled.div<{ imgSrc: string; width: string }>`
   border-radius: 24px;
 `;
 
+export const Description = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 18px;
+`;
+
+export const TextWrapper = styled.div`
+  gap: 18px;
+  background: transparent;
+  border-radius: 24px;
+  border: 1px solid #353535;
+
+  padding: 25px;
+  width: calc(100% - 50px);
+
+  color: #cde6f1;
+
+  font-family: PF Videotext Pro;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+  @media (max-width: 576px) {
+    font-size: 15px;
+  }
+  @media (min-width: 577px) {
+    font-size: 20px;
+  }
+  @media (min-width: 769px) {
+    font-size: 24px;
+  }
+  @media (min-width: 993px) {
+    font-size: 15px;
+  }
+  @media (min-width: 1201px) {
+    font-size: 15px;
+  }
+  @media (min-width: 1401px) {
+    font-size: 15px;
+  }
+`;
+
 export const HackatonCard = styled.div`
-  width: calc(100% - 56px);
+  width: 100%;
 
   background-color: rgba(196, 196, 196, 0.01);
-  box-shadow: 0px 66px 100px 0px rgba(13, 36, 245, 0.25) inset,
-    0px 4px 18px 0px #580df5 inset;
-  -webkit-backdrop-filter: blur(50px);
+
+  box-shadow: 0px 66px 100px 0px rgba(45, 198, 105, 0.25) inset,
+    0px 4px 18px 0px #2dc669 inset;
   backdrop-filter: blur(50px);
   border-radius: 24px;
 
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
-  align-items: flex-start;
+  align-items: center;
   gap: 18px;
 
   @media (max-width: 576px) {
     gap: 18px;
     padding-top: 17px;
     padding-bottom: 28px;
-    padding-left: 28px;
-    padding-right: 28px;
   }
   @media (min-width: 577px) {
     gap: 24px;
     padding-top: 22px;
     padding-bottom: 33px;
-    padding-left: 33px;
-    padding-right: 33px;
   }
   @media (min-width: 769px) {
     gap: 28px;
     padding-top: 27px;
     padding-bottom: 38px;
-    padding-left: 38px;
-    padding-right: 38px;
   }
   @media (min-width: 993px) {
     gap: 30px;
     padding-top: 27px;
     padding-bottom: 38px;
-    padding-left: 38px;
-    padding-right: 38px;
   }
   @media (min-width: 1201px) {
     gap: 30px;
     padding-top: 27px;
     padding-bottom: 38px;
-    padding-left: 38px;
-    padding-right: 38px;
   }
   @media (min-width: 1401px) {
     gap: 30px;
     padding-top: 27px;
     padding-bottom: 38px;
-    padding-left: 38px;
-    padding-right: 38px;
   }
 `;
 
